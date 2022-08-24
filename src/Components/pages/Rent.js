d0e2535d4253ea4c9ad9ee478880e15857589dcd
Shobhit Jaryal <jaryalshaurya2203@gmail.com>
2022-08-24 23:16:16 +0530

    react-select added

--- src/Components/pages/Rent.js
+++ src/Components/pages/Rent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, Typography, TextField } from "@mui/material";
+import RentFilters from "./SearchFilters/RentFilters";
 
 function Rent() {
   return (
@@ -38,7 +39,9 @@ function Rent() {
       <section>
         <Grid container my={4}>
           <Grid item md={3} xs={12}>
-            
+              <center>
+                <RentFilters />
+              </center>
           </Grid>
         </Grid>
       </section>
---
+++ src/Components/pages/SearchFilters/RentFilters.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RentFilters() {
+  return (
+    <div>RentFilters</div>
+  )
+}
+
+export default RentFilters
