e6a54ff082eff822b74566f51fcdfc2a8161bb45
Shobhit Jaryal <jaryalshaurya2203@gmail.com>
2022-08-24 23:04:45 +0530

    title added

--- src/Components/routes/Routes.js
+++ src/Components/routes/Routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Rent from "../pages/Rent";
 
 function MyRoutes() {
   return (
     <Routes>
-      <Route path="/Rent" element={<h1>Home</h1>} />
+      <Route path='/' element={<Rent />} />
+      <Route path='/Rent' element={<Rent />} />
       <Route path="/*" element={<h1>Nothing to see here</h1>} />
     </Routes>
   );
