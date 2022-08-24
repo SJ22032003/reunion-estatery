c6e7c5701302e3aa851775f11c5a4517f1affe1c
Shobhit Jaryal <jaryalshaurya2203@gmail.com>
2022-08-24 21:16:57 +0530

    navbar added

--- src/Components/routes/Routes.js
+++ src/Components/routes/Routes.js
@@ -4,7 +4,8 @@ import { Routes, Route } from "react-router-dom";
 function MyRoutes() {
   return (
     <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/Rent" element={<h1>Home</h1>} />
+      <Route path="/*" element={<h1>Nothing to see here</h1>} />
     </Routes>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+const rootNode = document.getElementById("root");
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
+  rootNode
 );
