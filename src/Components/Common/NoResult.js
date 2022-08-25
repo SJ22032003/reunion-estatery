b1ab6e0192c888d2dd803a3c60b301829637489e
Shobhit Jaryal <jaryalshaurya2203@gmail.com>
2022-08-25 21:41:21 +0530

    data config and css fixes

--- src/Components/Common/NoResult.js
+++ src/Components/Common/NoResult.js
@@ -3,7 +3,7 @@ import noResult from "../assets/noResult.png";
 
 function NoResult() {
   return (
-    <div>
+    <div style={{minHeight:'32vh'}}>
       <center>
         <img src={noResult} alt="no result :(" />
       </center>
